3325a225cdc8217a7494d9864187ea0735b7abf2
Jede Brahma <brahmajede@gmail.com>
2021-01-05 19:08:14 +0000

    Automatically backed up by Learn

--- lib/math.rb
+++ lib/math.rb
@@ -19,31 +19,3 @@ end
      puts math.sqrt(num.to_f)
   end
 end
-calculator = Calculator.new
-
-puts "What would you like to do? Choose : \ 'add' for Addition\ 'sub' for subtraction\ 'div' for division\ 'mult' for product\ 'modulo' for percentage\ 'sqrt' for square-root"
-method = gets.chomp
-
-puts "Enter first number: "
-num1 = gets.to_f
-
-puts "Enter the second number:"
-num2 = gets.to_f
-
-if method == "add"
-  calculator.add(num1, num2)
-elseif method == "sub"
-  calculator.sub(num1,num2)
-elseif method == "div"
-  calculator.div(num1,num2)
-elseif method == "mult"
-  calculator.mult(num1,num2)
-elseif method == "modulo"
-  calculator.modulo(num1,num2)
-elseif method == "sqrt"
-  calculator.sqrt(num)  
-else
-  puts "Method unknown"
-  
-  end
-end  
