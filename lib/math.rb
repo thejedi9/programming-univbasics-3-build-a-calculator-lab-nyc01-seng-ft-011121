aa4e62b7a0906b0724648041bb3b563f0176ed40
Jede Brahma <brahmajede@gmail.com>
2021-01-05 18:00:59 +0000

    Automatically backed up by Learn

--- lib/math.rb
+++ lib/math.rb
@@ -1,23 +1,49 @@
-def addition(num1, num2)
-
+class Calculator
+end  
+  def add(num1, num2)
+     puts num1+num2
+  end
+  def sub(num1, num2)
+     puts num1-num2
+  end
+  def div(num1, num2)
+     puts num1.to_f/num2.to_f
+  end
+  def mult(num1, num2)
+     puts num1*num2
+  end
+  def modulo(num1, num2)
+     puts num1%num2
+  end
+  def sqrt(num)
+     puts math.sqrt(num.to_f)
+  end
 end
+RubyCalc = Calculator.new
 
-def subtraction(num1, num2)
+puts "What would you like to do? Choose : \ 'add' for Addition\ 'sub' for subtraction\ 'div' for division\ 'mult' for product\ 'modulo' for percentage\ 'sqrt' for square-root"
+method = gets.chomp
 
+puts "Enter first number: "
+num1 = gets.to_f
+
+puts "Enter the second number:"
+num2 = gets.to_f
+
+if method == "add"
+  RubyCalc.add(num1, num2)
+elseif method == "sub"
+  RubyCalc.sub(num1,num2)
+elseif method == "div"
+  RubyCalc.div(num1,num2)
+elseif method == "mult"
+  RubyCalc.mult(num1,num2)
+elseif method == "modulo"
+  RubyCalc.modulo(num1,num2)
+elseif method == "sqrt"
+  RubyCalc.sqrt(num)  
+else
+  puts "Method unknown"
+  
 end
-
-def division(num1, num2)
-
-end
-
-def multiplication(num1, num2)
-
-end
-
-def modulo(num1, num2)
-
-end
-
-def square_root(num)
-
-end
+  
