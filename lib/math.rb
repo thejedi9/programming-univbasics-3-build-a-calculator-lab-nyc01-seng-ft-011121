e540aacf0f4a0cedae4d71a826be1af07d82edfa
Jede Brahma <brahmajede@gmail.com>
2021-01-05 18:46:45 +0000

    Automatically backed up by Learn

--- lib/math.rb
+++ lib/math.rb
@@ -4,9 +4,9 @@ end
      puts num1+num2
   end
   def sub(num1, num2)
-     puts num1-num2
+     puts num2-num1
   end
-  def div(num1, num2)
+  def div(num2, num1)
      puts num1.to_f/num2.to_f
   end
   def mult(num1, num2)
